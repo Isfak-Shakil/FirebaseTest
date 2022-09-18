270d8960214f383d0dcf854d9db12f183ac45b40
USER <isfakshakil@gmail.com>
2022-09-19 00:38:30 +0600

    date time pick in am/pm

--- app/src/main/java/com/example/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.firebasedemo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -15,30 +16,33 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-       TextView textView1,textView2,getDateAndTime;
-       Calendar calendar;
-       SimpleDateFormat simpleDateFormat;
-       String date;
+       TextView textView1,textView2,tv3,tv4;
+//       Calendar calendar;
+//       SimpleDateFormat simpleDateFormat;
+//       String date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView1=findViewById(R.id.tv1);
         textView2=findViewById(R.id.tv2);
-       getDateAndTime=findViewById(R.id.tv3);
+        tv3=findViewById(R.id.tv3);
+        tv4=findViewById(R.id.tv4);
+        tv3.setText(getCurrentTime());
+        tv4.setText(getCurrentDate());
+
+//       calendar=Calendar.getInstance();
+//       simpleDateFormat=new SimpleDateFormat("dd-MM-yyyy HH:mm");
+//       date=simpleDateFormat.format(calendar.getTime());
+//       getDateAndTime.setText(date);
 
 
-       calendar=Calendar.getInstance();
-       simpleDateFormat=new SimpleDateFormat("dd-MM-yyyy HH:mm");
 
-       date=simpleDateFormat.format(calendar.getTime());
-
-       getDateAndTime.setText(date);
-
-
-        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users");
+       DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users");
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -53,6 +57,11 @@ public class MainActivity extends AppCompatActivity {
                     textView1.setText(a);
                     textView2.setText(ab);
 
+                    if (a.equals(tv3) && ab.equals(tv4)){
+                        startActivity(new Intent(MainActivity.this,Lock.class));
+                        finish();
+                    }
+
                 //}
             }
 
@@ -62,5 +71,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @SuppressLint("SimpleDateFormat")
+    private String getCurrentTime(){
+        return new SimpleDateFormat("hh:mm a", Locale.getDefault()).format(new Date());
+    }
+    @SuppressLint("SimpleDateFormat")
+    private String getCurrentDate(){
+        return new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
     }
 }

3ab58ca39a35454eb9c2669a9b89885cf246ab69
USER <isfakshakil@gmail.com>
2022-09-20 12:06:16 +0600

    compareing date,time with server date,time

--- app/src/main/java/com/example/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,6 +15,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -21,9 +26,8 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
        TextView textView1,textView2,tv3,tv4;
-//       Calendar calendar;
-//       SimpleDateFormat simpleDateFormat;
-//       String date;
+       String currentDate,currentTime,serverTime,serverDate;
+    Date date1,date2,time1,time2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,52 +36,60 @@ public class MainActivity extends AppCompatActivity {
         textView2=findViewById(R.id.tv2);
         tv3=findViewById(R.id.tv3);
         tv4=findViewById(R.id.tv4);
-        tv3.setText(getCurrentTime());
-        tv4.setText(getCurrentDate());
-
-//       calendar=Calendar.getInstance();
-//       simpleDateFormat=new SimpleDateFormat("dd-MM-yyyy HH:mm");
-//       date=simpleDateFormat.format(calendar.getTime());
-//       getDateAndTime.setText(date);
 
 
+        currentTime=getCurrentTime();
+        currentDate=getCurrentDate();
 
-       DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users");
+        tv3.setText(currentTime);
+        tv4.setText(currentDate);
+
+        puData();
+
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users");
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-               // for (DataSnapshot ds: snapshot.getChildren()){
-//                    String a=""+ds.child("date").getValue();
-//                    String ab=""+ds.child("time").getValue();
-//                    textView1.setText(a);
-//                    textView2.setText(ab);
+                 serverTime=snapshot.child("time").getValue().toString();
+                 serverDate=snapshot.child("date").getValue().toString();
 
-                    String a=snapshot.child("date").getValue().toString();
-                    String ab=snapshot.child("time").getValue().toString();
-                    textView1.setText(a);
-                    textView2.setText(ab);
+                try {
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                     date1 = sdf.parse(serverDate);
+                     date2 = sdf.parse(currentDate);
 
-                    if (a.equals(tv3) && ab.equals(tv4)){
+                    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a") ;
+                     time1 = timeFormat.parse(serverTime);
+                     time2=timeFormat.parse(currentTime);
+                    if (date2.after(date1)) {
                         startActivity(new Intent(MainActivity.this,Lock.class));
-                        finish();
+                    } else {
+                        if (time2.after(time1))
+                        {
+                            startActivity(new Intent(MainActivity.this,Lock.class));
+                        }
                     }
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                textView1.setText(serverTime);
+                textView2.setText(serverDate);
 
-                //}
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
+    }
 
-    }
-    @SuppressLint("SimpleDateFormat")
+    private void puData() {
+        DatabaseReference dref= FirebaseDatabase.getInstance().getReference("Users");
+        dref.child("date").setValue("20-09-2022");
+        dref.child("time").setValue("11:50 AM");}
+
     private String getCurrentTime(){
-        return new SimpleDateFormat("hh:mm a", Locale.getDefault()).format(new Date());
-    }
-    @SuppressLint("SimpleDateFormat")
+        return new SimpleDateFormat("hh:mm a", Locale.getDefault()).format(new Date());}
     private String getCurrentDate(){
-        return new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
-    }
+        return new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());}
 }
